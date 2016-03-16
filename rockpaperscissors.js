e3c63c0c75c2b4777d0e75e2b61adae655828fbf
psyfffer <404shaff@gmail.com>
2016-03-16 13:17:00 -0400

    Added files via upload

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -25,6 +25,7 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
+    // GITHUB PUSH practice
     var PlayerMove = (move && null);
     if (PlayerMove === null) {
         PlayerMove = getInput();
